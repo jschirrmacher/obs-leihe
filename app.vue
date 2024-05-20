aba890b244f3253170999236e02555679dacc8eb
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2024-05-20 12:31:19 +0200

    feat: link to github repo and issues

--- app.vue
+++ app.vue
@@ -5,8 +5,15 @@
   <UContainer class="mt-4">
     <NuxtPage />
   </UContainer>
-
-  <NuxtPage />
+  <UContainer class="mt-4 pb-4 align-middle flex justify-end">
+    <UButton to="https://github.com/jschirrmacher/obs-leihe" target="_blank" variant="link">
+      <UAvatar src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" />
+      Repository
+    </UButton>
+    <UButton to="https://github.com/jschirrmacher/obs-leihe/issues/new" target="_blank" variant="link"
+      >Fehler melden</UButton
+    >
+  </UContainer>
 
   <UNotifications />
 </template>
